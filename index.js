05e29f52624de641b9648663840b2cd8c392a83e
zhifeng ou <zhifengou@hotmail.com>
2019-03-15 19:59:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+const token = 'ec558943f119c33be42b0db342a59c38dc2b3c29'
+  
+fetch('https://api.github.com/user/repos',{
+  headers:{
+    Authorization: `token ${token}`
+  }
+}).then(res=>res.json()).then(json=>console.log(json))
